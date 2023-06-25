3a6b91b283dcc6668098a3856b4a21b23045ceb3
TalJa1 <91865008+TalJa1@users.noreply.github.com>
2023-06-25 15:37:03 +0700

    Fix: Repository

--- Repository/Models/Class.cs
+++ Repository/Models/Class.cs
@@ -8,6 +8,7 @@ namespace Repository.Models
         public Class()
         {
             Attendances = new HashSet<Attendance>();
+            ClassChangeRequests = new HashSet<ClassChangeRequest>();
             Enrollments = new HashSet<Enrollment>();
             EventRequests = new HashSet<EventRequest>();
             Payments = new HashSet<Payment>();
@@ -17,15 +18,20 @@ namespace Repository.Models
         public string ClassName { get; set; } = null!;
         public int? InstructorId { get; set; }
         public int? RoomId { get; set; }
-        public DateTime StartTime { get; set; }
-        public DateTime EndTime { get; set; }
+        public DateTime BeginDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public TimeSpan StartTime { get; set; }
+        public TimeSpan EndTime { get; set; }
         public int Capacity { get; set; }
         public bool IsApproved { get; set; }
+        public int SlotId { get; set; }
         public decimal MoneyNeedToPay { get; set; }
 
         public virtual Instructor? Instructor { get; set; }
         public virtual Room? Room { get; set; }
+        public virtual Slot Slot { get; set; } = null!;
         public virtual ICollection<Attendance> Attendances { get; set; }
+        public virtual ICollection<ClassChangeRequest> ClassChangeRequests { get; set; }
         public virtual ICollection<Enrollment> Enrollments { get; set; }
         public virtual ICollection<EventRequest> EventRequests { get; set; }
         public virtual ICollection<Payment> Payments { get; set; }
---
+++ Repository/Models/ClassChangeRequest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Repository.Models
+{
+    public partial class ClassChangeRequest
+    {
+        public int RequestId { get; set; }
+        public int? MemberId { get; set; }
+        public int? ClassId { get; set; }
+        public DateTime RequestDate { get; set; }
+        public bool? IsApproved { get; set; }
+
+        public virtual Class? Class { get; set; }
+        public virtual Member? Member { get; set; }
+    }
+}
