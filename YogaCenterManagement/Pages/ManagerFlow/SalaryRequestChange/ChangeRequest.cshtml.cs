d5a892e43b584998967da6f2d23c95332a054d90
Nguyen Toan <toanmnh2002@gmail.com>
2023-06-28 15:58:17 +0700

    update

---
+++ YogaCenterManagement/Pages/ManagerFlow/SalaryRequestChange/ChangeRequest.cshtml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Threading.Tasks;
+using FluentValidation;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Repository.DAO;
+using Repository.Models;
+
+namespace YogaCenterManagement.Pages.ManagerFlow.SalaryRequestChange
+{
+    public class ChangeRequestModel : PageModel
+    {
+        private readonly InstructorService _instructorService;
+        private readonly SalaryChangeRequestService _salaryChangeRequestService;
+        private readonly MemberService _memberService;
+
+
+        public ChangeRequestModel(InstructorService instructorService, SalaryChangeRequestService salaryChangeRequestService)
+        {
+            _instructorService = instructorService;
+            _salaryChangeRequestService = salaryChangeRequestService;
+        }
+
+        public async Task<IActionResult> OnGetAsync()
+        {
+            var email=HttpContext.Session.GetString("email");
+            if (_salaryChangeRequestService.GetAll() != null)
+            {
+                SalaryChangeRequest = _salaryChangeRequestService.GetAll(include: x => x.Include(a => a.Instructor).ThenInclude(b => b.Member));
+            }
+            return Page();
+        }
+        [BindProperty]
+        public bool IsApproved { get; set; }
+        [BindProperty]
+        public IList<SalaryChangeRequest> SalaryChangeRequest { get; set; }
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync(int requestId,int instructorId, bool isApproved)
+        {
+            var request = _salaryChangeRequestService.getById(requestId);
+            var salaryChangeRequest = new SalaryChangeRequest();
+            salaryChangeRequest.RequestId = request.RequestId;
+            salaryChangeRequest.InstructorId = instructorId;
+            salaryChangeRequest.NewSalary = request.NewSalary;
+            if (isApproved)
+            {
+                salaryChangeRequest.IsApproved = true;
+            }
+            else
+            {
+                salaryChangeRequest.IsApproved = false;
+            }
+            _salaryChangeRequestService.Add(salaryChangeRequest);
+            return RedirectToPage("./SalaryChangeRequest");
+        }
+    }
+}
---
+++ YogaCenterManagement/Pages/ManagerFlow/SalaryRequestChange/InstructorList.cshtml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Repository.DAO;
+using Repository.Models;
+
+namespace YogaCenterManagement.Pages.ManagerFlow.SalaryRequestChange
+{
+    public class InstructorListModel : PageModel
+    {
+        private readonly InstructorService _instructorService;
+        private readonly MemberService _memberService;
+
+        public InstructorListModel(InstructorService instructorService, MemberService memberService)
+        {
+            _instructorService = instructorService;
+            _memberService = memberService;
+        }
+
+        public IList<Instructor> Instructor { get; set; } = default!;
+
+        public IActionResult OnGetAsync()
+        {
+            var email = HttpContext.Session.GetString("email");
+            var member = _memberService.GetAll().FirstOrDefault(m => m.Email.Equals(email));
+            var listInstructor = _instructorService
+         .GetAll(include: x => x.Include(i => i.Member));
+            return Page();
+        }
+        public async Task<IActionResult> OnPostAsync(int instructorId)
+        {
+            if (instructorId != null)
+            {
+                HttpContext.Session.SetInt32("InstructorId", instructorId);
+                return RedirectToPage("ChangeRequest");
+            }
+            return Page();
+        }
+    }
+}
