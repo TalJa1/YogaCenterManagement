bef1bcf0cc92b1ba55a7757e4588cb0dfc2c44b8
bao-chan <92993244+bao-chan@users.noreply.github.com>
2023-06-28 10:01:13 +0700

    fix check attendence

---
+++ YogaCenterManagement/Pages/InstructorFlow/Edit.cshtml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Repository.Models;
+
+namespace YogaCenterManagement.Pages.InstructorFlow
+{
+    public class EditModel : PageModel
+    {
+        private readonly Repository.Models.YogaCenterContext _context;
+
+        public EditModel(Repository.Models.YogaCenterContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Attendance Attendance { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null || _context.Attendances == null)
+            {
+                return NotFound();
+            }
+
+            var attendance =  await _context.Attendances.FirstOrDefaultAsync(m => m.AttendanceId == id);
+            if (attendance == null)
+            {
+                return NotFound();
+            }
+            Attendance = attendance;
+           ViewData["ClassId"] = new SelectList(_context.Classes, "ClassId", "ClassName");
+           ViewData["MemberId"] = new SelectList(_context.Members, "MemberId", "Address");
+            return Page();
+        }
+
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Attach(Attendance).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!AttendanceExists(Attendance.AttendanceId))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return RedirectToPage("./Index");
+        }
+
+        private bool AttendanceExists(int id)
+        {
+          return (_context.Attendances?.Any(e => e.AttendanceId == id)).GetValueOrDefault();
+        }
+    }
+}
